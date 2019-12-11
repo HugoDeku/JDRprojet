e206f263c22d5828800b4fbb45cc5b140e560bb5
ITEPRAT Hugo <iteprath@hostname.iut2.upmf-grenoble.fr>
2019-12-11 14:20:27 +0100

    Début class

--- mvc/model/Utilisateur.class.php
+++ mvc/model/Utilisateur.class.php
@@ -1,1 +1,43 @@
+<?php
 
+  class Utilisateur{
+
+    private $pseudo;
+    private $droit;
+    private $listePersonnages;
+
+    function __construct($pseudo, $droit = "aucun" ,$listePersonnages = NULL)
+    {
+      $this->pseudo = $pseudo;
+      $this->droit = $droit;
+      $this->listePersonnages = $listePersonnages;
+    }
+
+    public function getPseudo()
+    {
+      return $this->pseudo;
+    }
+
+    public function getListePersonnages()
+    {
+      return $this->listePersonnages;
+    }
+
+    public function getDroit()
+    {
+      return $this->droit;
+    }
+
+    public function addPersonnage(Personnage $personnage)
+    {
+
+    }
+
+
+  }
+
+
+
+
+
+ ?>
--- mvc/view/Header.view.php
+++ mvc/view/Header.view.php
@@ -1,7 +1,7 @@
 <header>
   <nav>
     <ul>
-      <li><a href="#">Accueil</a></li>
+      <li><a href="../view/Accueil.view.php">Accueil</a></li>
       <li><a href="#">Connexion</a></li>
       <li><a href="#">Création</a></li>
       <li><a href="#">Visualisation</a></li>
