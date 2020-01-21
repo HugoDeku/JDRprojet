671cd45128e0fa44102e7e91cbbb0bfaace95db1
HugoDeku <hucureuil@gmail.com>
2020-01-21 15:52:09 +0100

    j'ai réécris le create.sql

--- mvc/BD/create.sql
+++ mvc/BD/create.sql
@@ -1,20 +1,20 @@
-BEgIn;
-CreATE TabLe Compte(
-  Pseudo VArChAR(30) PrImAry KEy,
-  Mdp TeXT
+BEGIN;
+CREATE TABLE Compte(
+  Pseudo VARCHAR(30) PRIMARY KEY,
+  Mdp TEXT
 );
 
-CreAte TABLe Dieu(
+CREATE TABLE Dieu(
   NomDieu VARCHAR(30) PRIMARY KEY
 );
 
-CreATe TabLe Magie(
-  NomMagie VaRchAr(30) PrIMaRY KeY,
-  Affinite VArChAR(30),
-  ForEIGN kEY Affinite REfeREnces Dieu(NomDieu)
+CREATE TABLE Magie(
+  NomMagie VARCHAR(30) PRIMARY KEY,
+  Affinite VARCHAR(30),
+  FOREIGN KEY Affinite REFERENCES Dieu(NomDieu)
 );
 
-CreATE TabLE Personnage(
+CREATE TABLE Personnage(
   Nom VARCHAR(30),
   Prenom VARCHAR(30),
   Age INTEGER,
@@ -22,52 +22,43 @@ CreATE TabLE Personnage(
   Sexe VARCHAR(30) CHECK (Sexe = "Homme" OR Sexe="Femme"),
   Taille FLOAT,
   Poids FLOAT,
-  PNJ VArChAR(1) CHECK(PNJ = "T" OR PNJ = "F"),
+  PNJ VARCHAR(1) CHECK(PNJ = "T" OR PNJ = "F"),
   Race VARCHAR(30),
   Lore TEXT,
-  PrImArY KeY (Nom,Prenom),
-  FoReiGn KeY Magie REfeREnces Magie(NomMagie)
+  PRIMARY KEY (Nom,Prenom),
+  FOREIGN KEY Magie REFERENCES Magie(NomMagie)
 );
 
-CreAte TAbLe Race(
-  Nom VArChAR(30) pRimARY kEy,
-  TailleMoy FlOAT,
-  PoidsMoy Float,
-  Description TExT
+CREATE TABLE Race(
+  Nom VARCHAR(30) PRIMARY KEY,
+  TailleMoy FLOAT,
+  PoidsMoy FLOAT,
+  Description TEXT
 );
 
-cREATE TabLE JoueurPerso(
-  Pseudo VArChAR(30),
+CREATE TABLE JoueurPerso(
+  Pseudo VARCHAR(30),
   Nom VARCHAR(30),
   Prenom VARCHAR(30),
-  FoREiGN KEY (Pseudo) REfeREnces Compte(Pseudo),
-  FoREiGN KEY (Nom, Prenom) REfeREnces Personnage(Nom, Prenom),
-  PrImArY Key (Pseudo, Nom, Prenom)
+  FOREIGN KEY (Pseudo) REFERENCES Compte(Pseudo),
+  FOREIGN KEY (Nom, Prenom) REFERENCES Personnage(Nom, Prenom),
+  PRIMARY KEY (Pseudo, Nom, Prenom)
 );
 
-CreAte TABle Stats(
-  NomPers VArChAR(30),
-  PrenomPErs VArChAR(30),
-  PVMax Integer,
-  ManaMax Integer,
-  PV Integer,
-  Mana IntEger
+CREATE TABLE Stats(
+  NomPers VARCHAR(30),
+  PrenomPErs VARCHAR(30),
+  PVMax INTEGER,
+  ManaMax INTEGER,
+  PV INTEGER,
+  Mana INTEGER
 );
 
-CreAte TaBle Spells(
-  NomMagie VaRchAr(30),
-  NomSpell VArchAr(30) PRIMARY KEY,
+CREATE TABLE Spells(
+  NomMagie VARCHAR(30),
+  NomSpell VARCHAR(30) PRIMARY KEY,
   DescSpell TEXT,
   FOREIGN KEY NomMagie REFERENCES Magie(NomMagie)
 );
 
-
-
-
-
-
-
-
-
-
-COmmiT;
+COMMIT;
